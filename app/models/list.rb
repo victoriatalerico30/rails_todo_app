250e32fcd14720579371cb7b99647b060f80e537
Victoria Talerico <victoriatalerico@hotmail.com>
2016-11-24 12:47:38 -0300

    agrgegado test de lista

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,5 +1,8 @@
 class List < ApplicationRecord
 	has_many :tasks
+
 	validates :title , presence: true , uniqueness: true
 	validates :url , presence: true , uniqueness: true
+
+	
 end
--- test/models/list_test.rb
+++ test/models/list_test.rb
@@ -1,7 +1,27 @@
 require 'test_helper'
 
 class ListTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "if title is empty, it should be invalid" do
+  	list = List.new
+  	list.url= 'urlTest.com'
+    assert_not list.valid?
+  end
+
+   test "if url is empty, it should be invalid" do
+  	list = List.new
+  	list.title= 'Lista1'
+    assert_not list.valid?
+  end
+
+   test "if url is not unique, it should be invalid" do
+  	list = List.new 
+  	list.title='Lista1'
+  	list.url= 'urlTest.com'
+  	list.save
+  	list2 = List.new 
+  	list2.title='Lista2'
+  	list2.url= 'urlTest.com'
+    assert_not list2.valid?
+  end
+
 end
